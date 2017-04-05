d79eefbd326661b9c2cd4a221d983ed4e5525884
chucuoinb <laivannamyknb@gmail.com>
2017-04-06 01:48:47 +0700

    css

---
+++ includes/inc_header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+</head>
+<body>
+<div id="inc_header">
+    <div class="container" id="header_center">
+        <div id="header_logo">
+                <span id="inc_header_title1">Bis</span><span id="inc_header_title2">Lite</span>
+        </div>
+        <div id="menu">
+            <ul>
+                <li><a>HOME</a></li>
+                <li><a>ABOUT US</a></li>
+                <li><a>SERVICES</a><i class="fa fa-angle-down" aria-hidden="true"></i></li>
+                <li><a>PORTFOLIO</a><i class="fa fa-angle-down" aria-hidden="true"></i></li>
+                <li><a>BLOG</a></li>
+                <li><a>CONTACT US</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+</body>
+</html>
