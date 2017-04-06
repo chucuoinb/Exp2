4246a8992271376630ab8854e817c893be46f734
chucuoinb <laivannamyknb@gmail.com>
2017-04-06 10:56:37 +0700

    home

---
+++ home/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/css_main.css">
+</head>
+<body>
+<?php
+    include ("../includes/inc_header.php");
+    include ("../includes/inc_home.php");
+    include ("../includes/inc_footer.php");
+?>
+</body>
+</html>
